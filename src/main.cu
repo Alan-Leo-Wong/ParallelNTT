6029eea5cfbede2a1c5289c8c44a873930434e7c
Poet <602289550@qq.com>
2023-06-04 14:58:07 +0800

    update folder

--- src/main.cu
+++ src/main.cu
@@ -4,19 +4,40 @@
 #include <iostream>
 #include <algorithm>
 
-int main(int argc, char **argv) {
-    int numIters = 5;
-    int n = 5, m = 5;
-    TEST_TYPE test_type = TEST_TYPE::CUDA;
-    if (argc > 1) {
-        std::string arg1 = argv[1];
-        std::transform(arg1.begin(), arg1.end(), arg1.begin(), ::tolower);  // 将参数转换为小写字母
-        if (arg1 == "cpu") test_type = TEST_TYPE::CPU;
-        else if(arg1 == "simd") test_type = TEST_TYPE::SIMD;
-        if (argc > 2) n = std::stoi(argv[2]);
-        if (argc > 3) m = std::stoi(argv[3]);
-        if (argc > 4) numIters = std::stoi(argv[4]);
+int numIters = 5;
+int n = 5, m = 5;
+TEST_TYPE test_type = TEST_TYPE::CUDA;
+
+// Parse the program parameters and set them as global variables
+void parseProgramParameters(int argc, char *argv[]) {
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        std::transform(arg.begin(), arg.end(), arg.begin(), ::tolower);  // 将参数转换为小写字母
+
+        if (arg == "-cpu") {
+            test_type = TEST_TYPE::CPU;
+        } else if (arg == "-iter") {
+            if (i + 1 < argc) {
+                numIters = std::stoi(argv[i + 1]);
+                ++i;
+            }
+        } else if (arg == "-n") {
+            if (i + 1 < argc) {
+                n = std::stoi(argv[i + 1]);
+                ++i;
+            }
+        } else if (arg == "-m") {
+            if (i + 1 < argc) {
+                m = std::stoi(argv[i + 1]);
+                ++i;
+            }
+        }
     }
+}
+
+int main(int argc, char **argv) {
+    parseProgramParameters(argc, argv);
+
     std::cout << "-- \033[0m\033[1;36m[INFO]\033[0m Using " << testTypeToString(test_type) << std::endl;
     std::cout << "-- \033[0m\033[1;36m[INFO]\033[0m Iterations = " << numIters << std::endl;
     std::cout << "-- \033[0m\033[1;36m[INFO]\033[0m n(多项式 A 次数的log) = " << n << std::endl;
